3799689227cc1f0e736880a9c463c85a683629c6
Damien Redd <damiens.redd@gmail.com>
2023-02-02 09:44:46 -0600

    Palindrome

--- challenge-is-palindrome/isPalindrome.js
+++ challenge-is-palindrome/isPalindrome.js
@@ -13,3 +13,22 @@ function checkPalindrome(string) {
 console.log(checkPalindrome('raCecaR'))
 
 
+const isPalindrome = str => {
+    for (let i = 0; i < str.length; i++){
+    let finalIndex = str.length - 1 - i
+
+    if(str[i] !== str[finalIndex]){
+        return false
+    }
+    }
+    return true
+}
+console.log(isPalindrome('tacocat'))
+console.log('========================')
+
+const isPalindromeMethods = str => {
+    let reverseStr = str.split('').reverse().join('')
+    return str === reverseStr
+}
+
+console.log(isPalindromeMethods('yahoo'))
