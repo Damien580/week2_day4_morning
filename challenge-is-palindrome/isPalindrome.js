6139e6aa1dd9a9647a6added73afb9414e31d2bd
Damien Redd <damiens.redd@gmail.com>
2023-02-02 09:26:22 -0600

    for loop solution

--- challenge-is-palindrome/isPalindrome.js
+++ challenge-is-palindrome/isPalindrome.js
@@ -1,1 +1,15 @@
 // Write your code below
+function checkPalindrome(string) {
+    for (i = 0; i < string.length / 2; i++){
+       string = string.toLowerCase()
+        if (string[i] !== string[string.length - 1 -i]){
+            return 'It\'s not a palindrome.'
+        }
+    }
+    return 'It\'s a palindrome.'
+
+}
+
+console.log(checkPalindrome('raCecaR'))
+
+
